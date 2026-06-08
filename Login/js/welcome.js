0f2c59eac438ce9eaa78ce0f6d83c1de46aa451d
Quitcrown <quitcrown@gmail.com>
2026-06-08 20:09:57 +0700

    Update welcome.js

--- Login/js/welcome.js
+++ Login/js/welcome.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function goLogin() {
-        window.location.href = "Login.html";
+        window.location.href = "Login/";
     }
 
     function logout() {
